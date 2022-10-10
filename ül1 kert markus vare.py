14d8fd3767b49dc480e1bbbf69b1caf411042e9b
kertmv <115462783+kertmv@users.noreply.github.com>
2022-10-10 13:46:50 +0300

    Add files via upload

---
+++ ül1 kert markus vare.py
@@ -0,0 +1,26 @@
+#kert markus vare
+#10-10-22
+# harjutus kilpkonn
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+aken. title("ülesanne 2 - KMV")
+
+
+
+#kujundi loomine
+k1 = turtle.Turtle()
+for i in range(5):
+  k1.fd(100)
+  k1.rt(144)
+  
+for i in range(3):
+   k1.color("red")
+   k1.backward(100)
+   k1.rt(120)
+  
+  
+  
+  
