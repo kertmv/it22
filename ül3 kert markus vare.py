492a90906508e2c14a46105b104ba8b94f14f626
kertmv <115462783+kertmv@users.noreply.github.com>
2022-10-10 14:08:19 +0300

    Add files via upload

---
+++ ül3 kert markus vare.py
@@ -0,0 +1,25 @@
+#kert markus vare
+#10-10-22
+# harjutus kilpkonn
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+aken. title("ülesanne  3 - KMV")
+
+k1 = turtle. Turtle()
+   #for j in range(4):
+   #k1. rt(90)
+   #for i in range(4):
+       #k1. rt(90)
+       #k1. fd(100)
+       #k1. rt(90)
+       #k1. fd(100)
+       #k1. lt(90)
+       #k1. fd(100)
+
+
+
+
+k1 = turtle.Turtle
