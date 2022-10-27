9fd408561feecd463d34679ca31fa8a37527e271
kertmv <115462783+kertmv@users.noreply.github.com>
2022-10-27 09:59:22 +0300

    Add files via upload

---
+++ kert harjutus 05.py
@@ -0,0 +1,51 @@
+#kert markus vare
+#25.10.22
+#harjutus05
+
+# vanused tärnidega
+vanused = [5,9l,10,5,18,16]
+ for in range 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#vanused
+Vanused =[15,54,69,65,4,65,65,48,21,87]
+print(f"Vanim {max(vanused)}")
+print(f"Noorim {min(vanused)}")
+print(f"Vanuste summa {sum(vanused)}")
+print(f"keskimine Vanus {sum(vanused)/len(vanused)}")
+
+
+
+
+
+
+
+
+
+#õpilaste nimede muutmine
+
+opilased =["juhan","kati","mario","mario","mati","mati"]
+puh_opilased = []
+for i in opilasad:
+    if i  in puh_opilased:
+        if  opilased.count(i) >= 2:
+            opilased.remove(i)
+            
+        
+     
+ 
+
+
+
